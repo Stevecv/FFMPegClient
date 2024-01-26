e22275d96a2f9c1d885ded471b80ac17cb2fc354
Stevecv <mail.stevecv@gmail.com>
2024-01-26 16:40:29 +0000

    bugfix

--- client.py
+++ client.py
@@ -4,14 +4,14 @@ server_ip = ""
 
 
 def get_sdp(video, port):
-    sdp_str = requests.get("http://" + server_ip + ":" + port + "/get-sdp?video-name=" + video + "&ip-address=" + server_ip).text
+    sdp_str = requests.get("http://" + server_ip + ":" + str(port) + "/get-sdp?video-name=" + video + "&ip-address=" + server_ip).text
     print(sdp_str)
 
 
 def video_chose_menu(port):
     video_list = requests.get("http://" + server_ip + ":" + str(port) + "/get-videos").text.split(",")
 
-    i = 0
+    i = 1
     for video in video_list:
         print(str(i) + ". " + video_list[i])
 
